a64acd56c715aa7b7a0616f3dbfc87b3165e80bf
Paul-1783 <paulschmitt144@gmail.com>
2025-06-10 17:40:37 +0200

    contributed additional styling

--- src/startScreen/start.js
+++ src/startScreen/start.js
@@ -9,34 +9,42 @@ export default function startScreen() {
         <section class="welcomeAdmiralDialog">
              <dialog class="newGameDialog">
               <form action="" method="dialog" id="newGameDialogForm">
-           <h1 class="navalHistoryHeader">Battleship - the Game</h1>
-                   <div class="wrapperFieldset">
-                    <fieldset>
+                <h1 class="navalHistoryHeader">Battleship - the Game</h1>
+                     <fieldset>
                     <legend>Player 1</legend>
                       <p>
                         <label for="player1Name">Player one, please enter your name:</label>
                         <input type="text" id="player1Name" autofocus value="Player 1" required>
                       </p>
                       <p>
-                        <label for="questionOne">Would you prefer an AI to play in your place?</label>
-                        <input type="checkbox" id="questionOne" name="decideAIOne" value="yes" checked />
+                        <span class="toggle-checkbox-wrapper">
+                          <label  for="toggle1">Would you prefer to have an AI play in your place?</label>
+                          <input class="toggle-checkbox" type="checkbox" id="toggle1">
+                            <label class="slider" for="toggle1">
+                              <span class="toggle-switch opt1">Yes</span>
+                              <span class="toggle-switch opt2">No</span>
+                            </label>
+                          </span>
                       </p>
                     </fieldset>
-                  </div>
-                  <wrapperFieldset>
-                    <fieldset>
+                     <fieldset>
                         <legend>Player 2</legend>
                         <p>
                           <label for="player2Name">Player two, please enter your name:</label>
                           <input type="text" id="player2Name" autofocus value="Player 2" required>
                         </p>
                         <p>
-                          <label for="questionTwo">Would you prefer an AI to play in your place?</label>
-                          <input type="checkbox" id="questionTwo" name="decideAITwo" value="yes" checked />
+                          <span class="toggle-checkbox-wrapper">
+                            <label class="secondaryLabel" for="toggle2">Would you prefer to have an AI play in your place?</label>
+                            <input class="toggle-checkbox" type="checkbox" id="toggle2">
+                              <label class="slider" for="toggle2">
+                                <span class="toggle-switch opt1">Yes</span>
+                                <span class="toggle-switch opt2">No</span>
+                              </label>
+                           </span>
                         </p>
-                                  </fieldset>
-                  </wrapperFieldset>
-                  <menu>
+                     </fieldset>
+                   <menu>
                        <button class="submitDialog">Start Game</button>
                   </menu>
               </form>
